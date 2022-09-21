a36304df3594108f8d1372e92071b042e4a11cfa
UlyssesZamora <leezam01@gmail.com>
2022-09-20 23:12:47 -0700

    added methods and tests for adding an assignment, deleting an assignment, and changing assignment name

--- cst438_gradebook/src/main/java/com/cst438/domain/AssignmentListDTO.java
+++ cst438_gradebook/src/main/java/com/cst438/domain/AssignmentListDTO.java
@@ -13,6 +13,9 @@ public class AssignmentListDTO {
 		public String dueDate;
 		public String courseTitle;
 		public int courseId;
+		
+		public AssignmentDTO() {}
+
 
 		public AssignmentDTO(int assignmentId, int courseId, String assignmentName, String dueDate,
 				String courseTitle) {
@@ -22,6 +25,7 @@ public class AssignmentListDTO {
 			this.dueDate = dueDate;
 			this.courseTitle = courseTitle;
 		}
+		
 
 		@Override
 		public String toString() {
